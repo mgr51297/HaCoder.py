1e5989314d28e30e24f734d9878bb13eed7f7aab
LukaSikic <laceratus37@gmail.com>
2016-07-08 16:10:46 +0200

    Update aes.py

--- aes.py
+++ aes.py
@@ -4,6 +4,6 @@
 import string
 import random
 
-#Renerate random 32 chars string for AES encryption
+#Generate random 32 chars string for AES encryption
 random = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
 print random
